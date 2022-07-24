55bdcdc1f8dbdb68e70164dd6feeac8616a6f415
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-24 13:59:03 +0530

    REFACTORED: printed array using generics

--- src/com/bridgelabz/javacoreconceptsgenerics/PrintArray.java
+++ src/com/bridgelabz/javacoreconceptsgenerics/PrintArray.java
@@ -2,27 +2,16 @@ package com.bridgelabz.javacoreconceptsgenerics;
 
 public class PrintArray {
 	public static void main(String[] args) {
-		int[] a = {1,2,3,4,5};
-		double[] b = {1.1,2.1,3.1,4.1,5.1};
-		char[] c = {'a','b','c','d','e'};
+		Integer[] a = {1,2,3,4,5};
+		Double[] b = {1.1,2.1,3.1,4.1,5.1};
+		Character[] c = {'a','b','c','d','e'};
 		toPrint(a);
 		toPrint(b);
 		toPrint(c);
 	}
-	private static void toPrint(int[] a) {
-		for(int x : a) {
-			System.out.println(x);
+	private static <E> void toPrint(E[] inputArray) {
+		for(E element : inputArray) {
+			System.out.println(element);
 		}	
 	}
-	private static void toPrint(double[] b) {
-		for(double y : b) {
-			System.out.println(y);
-		}
-	}
-	private static void toPrint(char[] c) {
-	for(char z : c) {
-		System.out.println(z);
-	}
-}
-
 }
